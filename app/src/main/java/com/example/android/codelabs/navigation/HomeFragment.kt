03297e3ff01d03a1d443ff324b10f34aec22767d
pham.trung.huong <FRAMGIA\pham.trung.huong@framgia0387-pc.framgia.com>
2019-04-26 16:41:57 +0700

    Handle deeplink

--- app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
+++ app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
@@ -39,14 +39,6 @@ class HomeFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //TODO STEP 5 - Set an OnClickListener, using Navigation.createNavigateOnClickListener()
-//        val button = view.findViewById<Button>(R.id.navigate_destination_button)
-//        button?.setOnClickListener {
-//            findNavController().navigate(R.id.flow_step_one_dest)
-//        }
-        //TODO END STEP 5
-
-        //TODO STEP 6 - Set NavOptions
         val options = navOptions {
             anim {
                 enter = R.anim.slide_in_right
@@ -58,9 +50,7 @@ class HomeFragment : Fragment() {
         view.findViewById<Button>(R.id.navigate_destination_button)?.setOnClickListener {
             findNavController().navigate(R.id.flow_step_one_dest, null, options)
         }
-        //TODO END STEP 6
 
-        //TODO STEP 7.2 - Update the OnClickListener to navigate using an action
         view.findViewById<Button>(R.id.navigate_action_button)?.setOnClickListener {
             val flowStepNumberArg = 2
             val action = HomeFragmentDirections.nextAction().apply {
@@ -69,7 +59,6 @@ class HomeFragment : Fragment() {
 //            Navigation.createNavigateOnClickListener(R.id.next_action, null)
             findNavController().navigate(action)
         }
-        //TODO END STEP 7.2
     }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
